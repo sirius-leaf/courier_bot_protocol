ad76054b2f785e5b3008c5d3554e35db721512a5
Sirius Leaf <hfzamdhai@gmail.com>
2025-01-12 11:00:25 +0700

    added more player type

--- Script/camera_2d.gd
+++ Script/camera_2d.gd
@@ -1,7 +1,9 @@
 extends Camera2D
 
-@onready var sd_570: Player = $"../SD-570"
+var _currentPlaye := 0
+var _player: Player
+
+@onready var player_1: Player = $"../Players/Player1"
 
 func _process(delta: float) -> void:
-	if sd_570.global_position.y < 0.0:
-		global_position.y = sd_570.global_position.y
+	global_position.y = player_1.global_position.y if player_1.global_position.y <= 0.0 else 0.0
--- Script/player.gd
+++ Script/player.gd
@@ -1,8 +1,11 @@
 extends CharacterBody2D
 class_name Player
 
-@export var moveDistance: float = 16.0
-@export var moveSpeed: float = 0.2
+const MOVING_THRESHOLD := 6.0
+
+@export var moveDistance := 16.0
+@export var moveSpeed := 0.15
+@export var smallPlayer := true
 
 var _playerPos := Vector2(0.0, 0.0)
 var _onMovingPlatform := false
@@ -11,41 +14,79 @@ var _onMovingPlatformOffsetX := 0.0
 var _movingPlatform: MovingObject
 var _onPlatformOffsetX: float
 var _onWater := false
+var _isMoving := true
+var _movingOnPlatform := false
+var _moveAngle := 270.0
 
-@onready var check_up: RayCast2D = $MovementCheckRaycast/CheckUp
-@onready var check_down: RayCast2D = $MovementCheckRaycast/CheckDown
+@onready var check_up: Area2D = $MovementCheckRaycast/CheckUp
+@onready var check_down: Area2D = $MovementCheckRaycast/CheckDown
 @onready var check_left: RayCast2D = $MovementCheckRaycast/CheckLeft
 @onready var check_right: RayCast2D = $MovementCheckRaycast/CheckRight
-
-func _ready() -> void:
-	print(OS.has_feature("web_android") or OS.has_feature("web_ios"))
+@onready var sprite: Sprite2D = $Sprite
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_up") and not check_up.is_colliding():
-		_playerPos.y -= moveDistance
-	elif Input.is_action_just_pressed("ui_down") and not check_down.is_colliding():
-		_playerPos.y += moveDistance
-	elif Input.is_action_just_pressed("ui_left") and not check_left.is_colliding():
-		if not _onMovingPlatform:
-			_playerPos.x -= moveDistance
-		else:
-			_playerPosOnMoving -= moveDistance
-	elif Input.is_action_just_pressed("ui_right") and not check_right.is_colliding():
-		if not _onMovingPlatform:
-			_playerPos.x += moveDistance
-		else:
-			_playerPosOnMoving += moveDistance
+	_isMoving = true if (
+		abs(global_position.y - _playerPos.y) > MOVING_THRESHOLD or
+		(not _onMovingPlatform and abs(global_position.x - _playerPos.x) > MOVING_THRESHOLD) or
+		(_onMovingPlatform and _movingOnPlatform)
+	) else false
+	
+	const MOVE_SCALE_CHANGE := 0.8
+	const MOVE_ROTATION_AMOUNT := 7.5
+	
+	if not _isMoving:
+		if Input.is_action_just_pressed("ui_up") and not check_up.has_overlapping_bodies():
+			_playerPos.y -= moveDistance
+			
+			if smallPlayer:
+				sprite.scale.y = MOVE_SCALE_CHANGE
+				_moveAngle = 270.0
+			else:
+				sprite.scale.x = 0.9
+		elif Input.is_action_just_pressed("ui_down") and not check_down.has_overlapping_bodies():
+			_playerPos.y += moveDistance
+			
+			if smallPlayer:
+				sprite.scale.y = MOVE_SCALE_CHANGE
+				_moveAngle = 90.0
+			else:
+				sprite.scale.x = 0.9
+		elif Input.is_action_just_pressed("ui_left") and not check_left.is_colliding():
+			if not _onMovingPlatform:
+				_playerPos.x -= moveDistance
+			else:
+				_playerPosOnMoving -= moveDistance
+			
+			if smallPlayer:
+				_moveAngle = 180.0
+				sprite.scale.y = MOVE_SCALE_CHANGE
+			else:
+				sprite.rotation_degrees = -MOVE_ROTATION_AMOUNT
+		elif Input.is_action_just_pressed("ui_right") and not check_right.is_colliding():
+			if not _onMovingPlatform:
+				_playerPos.x += moveDistance
+			else:
+				_playerPosOnMoving += moveDistance
+			
+			if smallPlayer:
+				_moveAngle = 0.0
+				sprite.scale.y = MOVE_SCALE_CHANGE
+			else:
+				sprite.rotation_degrees = MOVE_ROTATION_AMOUNT
 	
 	if _onWater and not _onMovingPlatform: get_tree().reload_current_scene()
 
 
 func _physics_process(delta: float) -> void:
 	global_position.y = lerp(global_position.y, _playerPos.y, moveSpeed)
+	sprite.rotation = lerp_angle(sprite.rotation, deg_to_rad(_moveAngle), 0.3) if smallPlayer else lerp_angle(sprite.rotation, 0.0, 0.1)
+	sprite.scale = lerp(sprite.scale, Vector2(1.0, 1.0), 0.1)
 	
 	if not _onMovingPlatform:
 		global_position.x = lerp(global_position.x, _playerPos.x, moveSpeed)
 	else:
 		_onMovingPlatformOffsetX = lerp(_onMovingPlatformOffsetX, _playerPosOnMoving, moveSpeed)
+		_movingOnPlatform = true if abs(_onMovingPlatformOffsetX - _playerPosOnMoving) > MOVING_THRESHOLD else false
 		global_position.x = _movingPlatform.global_position.x + _onPlatformOffsetX + _onMovingPlatformOffsetX
 		_playerPos.x = roundf(global_position.x / 16.0) * 16.0
 
